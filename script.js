69340625864e40123f56781da79b10a29c22a1cd
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2021-05-29 15:34:07 -0300

    Javascript module tests

---
+++ script.js
@@ -0,0 +1,6 @@
+import numeroAleatorio from './numeroAleatorio.js';
+import { areaQuadrado, perimetroQuadrado } from './quadrado.js';
+
+console.log(areaQuadrado(4));
+console.log(perimetroQuadrado(5));
+console.log(numeroAleatorio());
